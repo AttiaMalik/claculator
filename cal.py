992f57d7caa7ac2fd9c28aadc81bc6c3f3195901
AttiaMalik <147254016+AttiaMalik@users.noreply.github.com>
2025-05-24 01:00:31 -0700

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,38 @@
+def add(num1,num2):       #define functions
+    return num1+num2
+def subtract(num1,num2):
+    return num1-num2
+def multiply(num1,num2):
+    return num1*num2
+def divide(num1,num2):
+    if num2 == 0:
+        print("Error! cannot divide with Zero ")      #Handle division by zero with appropriate error messages
+    return num1/num2
+
+while True:           # Calculator for Meun Loop
+    print("---Simple Calculator---")
+    print("1. Addition")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exist")
+
+    choice = int(input("Which Operation you performed? (1-5) "))
+    if choice == 5:
+        print("Exiting calculator. Goodbye!")
+        break
+
+    try:
+        num1 = float(input("Enter a first number: "))      #take input from user
+        num2 = float(input("Enter a second number: "))
+    except ValueError:
+        print("Please enter valid numbers.")
+        continue
+    if choice == 1:
+        print(f"Addition of two numbers: {num1+num2}")        # Apply conditions
+    elif choice == 2:
+        print(f"Subtract of two number: {num1-num2}")
+    elif choice == 3:
+        print(f"Multiply of two number: {num1*num2}")
+    elif choice == 4:
+        print(f"Divide of two number: {num1/num2}")
